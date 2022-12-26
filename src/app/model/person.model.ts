6d541f606d45d5b9976f39303f5320ca3486609f
martin-b-07e5 <martin.bergagno@gmail.com>
2022-12-26 08:30:04 -0300

    person.model.ts

---
+++ src/app/model/person.model.ts
@@ -0,0 +1,13 @@
+export class Person {
+  // private personId?: Number;
+  firstName: String;
+  lastName: String;
+  email: String
+
+  // constructor
+  constructor(firstName: String, lastName: String, email: String) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+  }
+}
