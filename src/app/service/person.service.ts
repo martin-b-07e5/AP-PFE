a761e8d947e2f235954a3e34416f7df8aa21a3cf
martin-b-07e5 <martin.bergagno@gmail.com>
2022-12-26 09:00:16 -0300

    person.service.ts working

---
+++ src/app/service/person.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Person} from '../model/person.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonService {
+  URL = "http://localhost:8082/api/v1/person/";
+
+  constructor(private http: HttpClient) {
+  }
+
+  public findAll(): Observable<Person> {
+    // tiene que estar corriendo spring » sino no muestra
+    return this.http.get<Person>(this.URL + "findById/1");
+  }
+
+}
