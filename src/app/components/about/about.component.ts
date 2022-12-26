223419dcb10897d6baafadafe68e1223d1e5348b
martin-b-07e5 <martin.bergagno@gmail.com>
2022-12-26 10:36:27 -0300

    about.component modified

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Person} from 'src/app/model/person.model';
+import {PersonService} from "../../service/person.service";
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  person: Person = new Person("", "", "");
+
+  constructor(public personService: PersonService) {
+    this.personService.findAll().subscribe(data => {
+      this.person = data
+    })
+  }
+
+  ngOnInit(): void {
+    /*this.personService.findAll().subscribe(data => {
+      this.person = data
+    })*/
+  }
+
 
 }
