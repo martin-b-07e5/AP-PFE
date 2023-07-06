84279325c189aad414d17dbf9af668d1b1b6faed
martin-b-07e5 <martin.bergagno@gmail.com>
2023-07-05 22:25:15 -0300

    done experience-create.component.ts

--- src/app/components/experience/create/experience-create.component.ts
+++ src/app/components/experience/create/experience-create.component.ts
@@ -1,36 +1,48 @@
-import {Component, OnInit} from '@angular/core';
-import {Router} from "@angular/router";
-import {Experience} from "../../../model/experience.model";
-import {ExperienceService} from "../../../service/experience.service";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Experience } from '../../../model/experience.model';
+import { ExperienceService } from '../../../service/experience.service';
 
 @Component({
   selector: 'app-experience-create',
   templateUrl: './experience-create.component.html',
-  styleUrls: ['./experience-create.component.css']
+  styleUrls: ['./experience-create.component.css'],
 })
 export class ExperienceCreateComponent implements OnInit {
   name: string = '';
   description: string = '';
+  startedAt: Date = null;
+  finishedAt: Date = null;
+  experience: Experience = new Experience('', '', null);
 
-  constructor(private experienceService: ExperienceService, private router: Router) {
-  }
+  constructor(
+    private experienceService: ExperienceService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onCreate(): void {
-    const experience = new Experience(this.name, this.description);
+    const experience = new Experience(
+      this.name,
+      this.description,
+      this.experience.startedAt,
+      this.experience.finishedAt
+    );
 
     // add WITH some validation
     this.experienceService.add(experience).subscribe(
-      next => {
-        this.router.navigate(['']).then(r => r);
-      }, error => {
-        alert("add Experience Failed");
+      (next) => {
+        this.router.navigate(['']).then((r) => r); // Redirects to home page.
+      },
+      (error) => {
+        alert('add Experience Failed');
         // this.router.navigate(['']).then(r => r);
       }
     );
-
   }
 
+  onCancel(): void {
+    this.router.navigate(['']).then((r) => r); // Redirects to home page.
+  }
 }
