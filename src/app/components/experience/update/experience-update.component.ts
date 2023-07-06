87aec66128ca25f4cdee3b4faf4bea53f3d677c9
martin-b-07e5 <martin.bergagno@gmail.com>
2023-07-05 23:19:45 -0300

    done experience-update.component.ts and experience-update.component.html

--- src/app/components/experience/update/experience-update.component.ts
+++ src/app/components/experience/update/experience-update.component.ts
@@ -1,45 +1,49 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
-import {Experience} from "../../../model/experience.model";
-import {ExperienceService} from "../../../service/experience.service";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Experience } from '../../../model/experience.model';
+import { ExperienceService } from '../../../service/experience.service';
 
 @Component({
   selector: 'app-experience-update',
   templateUrl: './experience-update.component.html',
-  styleUrls: ['./experience-update.component.css']
+  styleUrls: ['./experience-update.component.css'],
 })
 export class ExperienceUpdateComponent implements OnInit {
-
   // // @ts-ignore
   experience: Experience = null;
 
-  constructor(private experienceService: ExperienceService,
-              private activatedRoute: ActivatedRoute,
-              private router: Router) {
-  }
+  constructor(
+    private experienceService: ExperienceService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
 
   // captures the id
   ngOnInit(): void {
     const id = this.activatedRoute.snapshot.params['id'];
     this.experienceService.detail(id).subscribe(
-      next => {
+      (next) => {
         this.experience = next;
       },
-      error => {
-        alert("update Failed");
+      (error) => {
+        alert('update Failed');
       }
-    )
+    );
   }
 
   onUpdate(): void {
     const id = this.activatedRoute.snapshot.params['id'];
     this.experienceService.update(id, this.experience).subscribe(
-      next => {
-        this.router.navigate(['']).then(r => r);
-      }, error => {
-        alert("Update error.");
+      (next) => {
+        this.router.navigate(['']).then((r) => r); // Redirects to home page.
+      },
+      (error) => {
+        alert('Update error.');
       }
-    )
+    );
   }
 
+  onCancel(): void {
+    this.router.navigate(['']).then((r) => r); // Redirects to home page.
+  }
 }
